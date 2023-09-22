e5be68880b2e439a3bb8653da31adaa73a9a8855
Juan Mario <manueljuan790@gmail.com>
2023-09-22 11:23:53 -0300

    segundo commit

---
+++ src/Clases/ObjArmadura.java
@@ -0,0 +1,22 @@
+
+package Clases;
+
+
+public abstract class ObjArmadura {
+    public float consumo;
+
+    public ObjArmadura() {
+        this.consumo = 0;
+    }
+
+    public float getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(float consumo) {
+        this.consumo = consumo;
+    }
+    
+   
+    
+}
